e8dcf998fa61755a8b156b32283ad0f207293f64
1F10240016 <s1f102400167@iniad.org>
2025-07-09 13:45:39 +0900

    requirements.txtを追加

--- todo/tests.py
+++ todo/tests.py
@@ -9,7 +9,7 @@ class SampleTestCase(TestCase):
         self.assertEqual(1 + 2, 3)
 
 class TaskModelTestCase(TestCase):
-    def test_create_tasl1(self):
+    def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         task = Task(title='task1', due_at=due)
         task.save()
